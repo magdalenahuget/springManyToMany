06da22c9052ce839cb1e02a5d4430c1ba1d6bc43
magdalenahuget <magdalena.huget@gmail.com>
2023-10-10 13:45:38 +0200

    Initial commit

---
+++ src/main/java/com/company/bezkodermanytomany/BezkoderManyToManyApplication.java
@@ -0,0 +1,13 @@
+package com.company.bezkodermanytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezkoderManyToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BezkoderManyToManyApplication.class, args);
+	}
+
+}
