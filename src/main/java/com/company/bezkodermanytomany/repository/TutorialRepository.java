da1667190a372a10d9c2251d565298bb29ec264c
magdalenahuget <magdalena.huget@gmail.com>
2023-10-14 17:00:22 +0200

    Add controllers and repository implementation

--- src/main/java/com/company/bezkodermanytomany/repository/TutorialRepository.java
+++ src/main/java/com/company/bezkodermanytomany/repository/TutorialRepository.java
@@ -6,5 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
+
     List<Tutorial> findTutorialsByTagsId(Long tagId);
 }
